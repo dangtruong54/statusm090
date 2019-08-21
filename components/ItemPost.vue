e20d064bb24fe8d94f902390d0f1aab490ed2e3c
xa10-1 <truong.dang@tripath.vn>
2019-08-21 17:02:38 +0700

    first commit

---
+++ components/ItemPost.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="item-post">
+    <h4>Title item post</h4>
+    <p>It’s sometimes useful to emit a specific value with an event. For example, we may want the
+      component to be in charge of how much to enlarge the text by. In those cases, we can use $emit‘s 2nd parameter to provide this value:</p>
+    <a href="#" @click="statusElement(checkShow)">Detail post</a>
+    <div v-if="checkShow" class="More-detail">
+      <p>It’s sometimes useful to emit a specific value with an event. For example, we may want the
+        component to be in charge of how much to enlarge the text by</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ItemPost",
+    props: {
+      checkShow: {
+        type: Boolean
+      },
+    },
+    methods: {
+      statusElement(status) {
+        status = !status
+        this.$emit('currentClick', status)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .item-post p {
+    max-width: 1100px;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ItemPost :check-show="currentClick" @currentClick="clickParent($event)"/>
+  </div>
+</template>
+
+<script>
+import ItemPost from '../components/ItemPost'
+export default {
+  name: 'app',
+  components: {
+    ItemPost
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      currentClick: true
+    }
+  },
+  methods: {
+    clickParent(clickChild) {
+      this.currentClick = clickChild
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
